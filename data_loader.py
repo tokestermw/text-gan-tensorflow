cdfc3747e3619793f2ef20b3b297b54df583fa73
Motoki Wu <apewu@Motokis-MacBook-Pro.local>
2017-03-28 08:40:33 -0700

    added data loader that uses queue, should be simple enough

---
+++ data_loader.py
@@ -0,0 +1,132 @@
+""" Batch loader of PPDB data.
+
+Adapted from:
+    https://github.com/devsisters/pointer-network-tensorflow/blob/master/data_loader.py
+"""
+import os
+import time
+from contextlib import contextmanager
+from collections import Counter
+import threading
+
+import numpy as np
+import tensorflow as tf
+
+# -- local imports
+from utils import maybe_load
+
+
+DATA_DIR = "data"
+DATA_PATH = {
+    "ptb": {
+        "train": os.path.join(DATA_DIR, "ptb", "train.txt"),
+        "test": os.path.join(DATA_DIR, "ptb", "test.txt"),
+        "valid": os.path.join(DATA_DIR, "ptb", "valid.txt")
+    }
+}
+
+
+def tokenize(line):
+    return line.split()
+
+
+def read_data(path):
+    with open(path, 'r') as f:
+        for line in f:
+            line = line.strip()
+            yield line
+
+
+@maybe_load(save_path=os.path.join(DATA_DIR, "ptb", "vocab.pkl"))
+def build_vocab(path):
+    counts = Counter()
+
+    for line in read_data(path):
+        tokens = tokenize(line)
+        for token in tokens:
+            counts[token] += 1
+
+    word2idx = {word: idx for idx, (word, count) in enumerate(counts.most_common())}
+    idx2word = {idx: word for word, idx in word2idx.items()}
+
+    return word2idx, idx2word
+
+
+def vectorize(line, word2idx):
+    tokens = tokenize(line)
+    vector = [word2idx[token] for token in tokens]
+    return vector
+    # return np.array(vector, dtype=np.int32)
+
+
+def preprocess(data):
+    source = data[:, :-1]
+    target = data[:, 1:]
+    sequence_length = tf.reduce_sum(tf.not_equal(data, 0), axis=1) - 1
+    return source, target, sequence_length
+
+
+def get_and_run_input_queues(path, word2idx):
+    input_ph = tf.placeholder(tf.int32, shape=[None])  # [B, T]
+    queue = tf.PaddingFIFOQueue(shapes=[[None, ]], dtypes=[tf.int32], capacity=5000,)
+
+    enqueue_op = queue.enqueue([input_ph])
+    def enqueue_data(sess, epoch_size=10):
+        for epoch in range(epoch_size):
+            for idx, line in enumerate(read_data(path)):
+                v = vectorize(line, word2idx)
+                sess.run(enqueue_op, feed_dict={input_ph: v})
+
+    # queue_runner = tf.train.QueueRunner(queue, [enqueue_op] * 8)
+    # tf.train.add_queue_runner(queue_runner)
+
+    dequeue_op = queue.dequeue()
+    dequeue_batch = tf.train.batch([dequeue_op], batch_size=32, capacity=100, 
+        dynamic_pad=True, name="batch_and_pad")
+
+    return enqueue_data, dequeue_batch
+
+
+def start_threads(thread_fn, args, n_threads=8):
+    threads = []
+    for n in range(n_threads):
+        t = threading.Thread(target=thread_fn, args=args)
+        t.daemon = True # thread will close when parent quits
+        t.start()
+        threads.append(t)
+
+    time.sleep(1)  # enqueue a bunch before dequeue
+    return threads
+
+
+@contextmanager
+def queue_context(sess):
+    # thread coordinator
+    coord = tf.train.Coordinator()
+    try:
+        # start queue thread
+        threads = tf.train.start_queue_runners(sess, coord)
+        yield
+    except tf.errors.OutOfRangeError:
+        print("Done training")
+    except KeyboardInterrupt:
+        print("Force stop.")
+    finally:
+        # stop queue thread
+        coord.request_stop()
+        # wait thread to exit.
+        coord.join(threads)
+
+
+if __name__ == "__main__":
+    path = DATA_PATH["ptb"]["train"]
+    word2idx, idx2word = build_vocab(path)
+
+    with tf.Session() as sess:
+        enqueue_data, dequeue_batch = get_and_run_input_queues(path, word2idx)
+        threads = start_threads(enqueue_data, (sess, ))
+
+        with queue_context(sess):
+            while True:
+                print(sess.run(dequeue_batch).shape)
+
